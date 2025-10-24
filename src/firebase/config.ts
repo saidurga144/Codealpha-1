795f35961d3028a70df56f9e80098a6dd6a29371
Can i kiss you <canikiisu@gmail.com>
2025-10-24 13:22:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4091852394-d541a",
+  "appId": "1:126408820251:web:b484bf7a852a114f401abe",
+  "apiKey": "AIzaSyAYra-0lZU6odoFz3HONG0GGB6witOoznI",
+  "authDomain": "studio-4091852394-d541a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "126408820251"
+};
